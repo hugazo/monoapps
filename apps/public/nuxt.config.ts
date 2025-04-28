e0213b9be5f891d0a06948bf7433260af7b75b61
Hugo <hmoralesg@icloud.com>
2025-04-28 01:02:26 -0400

    feat: adds ionic

--- apps/public/nuxt.config.ts
+++ apps/public/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@mono/base-config'],
+  css: ['~/assets/css/ionic.css'],
   devtools: { enabled: true },
   compatibilityDate: '2024-11-01',
-});
+  modules: ['@nuxtjs/ionic'],
+  ssr: false,
+})
