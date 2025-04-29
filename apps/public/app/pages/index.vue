647490a27e4abdeabd2b49f7c7f5f1022c70e04f
Hugo <hmoralesg@icloud.com>
2025-04-29 03:20:42 -0400

    chore: added future templates

--- apps/public/app/pages/index.vue
+++ apps/public/app/pages/index.vue
@@ -5,8 +5,32 @@
         <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content class="ion-padding">
-      Hello World
+    <ion-content>
+      <ion-grid>
+        <ion-row class="ion-justify-content-center ion-align-items-center">
+          <ion-col>
+            <div>
+              <h1>
+                Hello World, this is a live reload test, now changing some shit
+              </h1>
+              <h2>
+                This should be a sub text
+              </h2>
+              <h3>
+                And this an h3333
+              </h3>
+              <ion-button router-link="/login">
+                Go to login
+              </ion-button>
+            </div>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
+    <ion-footer>
+      <ion-toolbar>
+        <ion-title>Footer</ion-title>
+      </ion-toolbar>
+    </ion-footer>
   </ion-page>
 </template>
---
+++ apps/public/app/pages/login.vue
@@ -0,0 +1,41 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <ion-grid class="flex flex-col h-full items-center justify-center">
+        <ion-row>
+          <ion-col class="text-center">
+            <h1 class="text-3xl font-bold">
+              Welcome Back
+            </h1>
+            <p class="text-sm text-gray-500">
+              Please login to your account
+            </p>
+          </ion-col>
+        </ion-row>
+        <ion-row class="w-full max-w-lg">
+          <ion-col>
+            <ion-input
+              v-model="email"
+              label="Email"
+              type="email"
+              placeholder="user@company"
+            />
+            <ion-input
+              v-model="password"
+              label="Password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script async setup lang="ts">
+const email = ref('');
+const password = ref('');
+
+// TODO: Implement login logic
+</script>
