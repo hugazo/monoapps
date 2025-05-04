a45a80bb384046a1ec6a66d8a9702d537547b374
Hugo <hmoralesg@icloud.com>
2025-05-04 05:11:11 -0400

    chore: updated dependencies

--- packages/base-config/nuxt.config.ts
+++ packages/base-config/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@vueuse/nuxt',
+    '@nuxt/image',
+  ],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
