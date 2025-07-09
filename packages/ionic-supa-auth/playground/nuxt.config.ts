10158d68cd303d6e491fb5d770865a3ddfa5b6f1
Hugo <hmoralesg@icloud.com>
2025-07-09 03:47:00 -0400

    chore: updated environment setup

--- packages/ionic-supa-auth/playground/nuxt.config.ts
+++ packages/ionic-supa-auth/playground/nuxt.config.ts
@@ -1,11 +1,6 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
-  ssr: false,
   devtools: {
     enabled: true,
-  },
-  supabase: {
-    url: 'https://your-supabase-url.supabase.co',
-    key: 'your-anon-key',
   },
 });
